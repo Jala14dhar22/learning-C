8e7d60f943719bba573ec35b114dba58889c884d
Jala14dhar22 <jaladharpadma22@gmail.com>
2024-03-19 22:30:26 +0530

    jaladhar

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){int a,b;
+    printf("enter value a:",a);
+    scanf("%d \n",&a);
+    printf("enter value b:",b);
+    scanf("%d \n",&b);
+
+    printf("sum =%d\n",a+b);
+return 0;}
